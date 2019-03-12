94360df740fd5292172e6dff0ecf88f02b651d3d
Abdul D Razak <abdul.d@telxsi.com>
2019-03-12 11:56:34 +0530

    footer is added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import NavBar from "./NavBar/Header";
 import DemoCarousel from "./Slider/Slider";
 import MainSec from "./Main/Main";
+import Footer from "./Footer/Footer";
+
 export default class Example extends React.Component {
   render() {
     return (
       <div>
         <NavBar />,
         <DemoCarousel />,
-        <MainSec />
+        <MainSec />,
+        <Footer />
       </div>
     );
   }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Row, Col } from "reactstrap";
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <Row>
+          <Col sm="3">
+            <h2>ABOUT US</h2>
+            <p>
+              {" "}
+              The first centralized IP camera, the Axis Neteye 200, was released
+              in 1996 by Axis Communications and was developed by the team of
+              Martin Gren and Carl-Axel Alm.[3] Though promoted based on its
+              direct accessibility from anywhere with an internet connection,[4]
+              the camera couldn't stream real-time motion video, but was limited
+            </p>
+          </Col>
+          <Col sm="3">
+            <h2>ABOUT US</h2>
+            <p>
+              {" "}
+              The first centralized IP camera, the Axis Neteye 200, was released
+              in 1996 by Axis Communications and was developed by the team of
+              Martin Gren and Carl-Axel Alm.[3] Though promoted based on its
+              direct accessibility from anywhere with an internet connection,[4]
+              the camera couldn't stream real-time motion video, but was limited
+            </p>
+          </Col>
+          <Col sm="3">
+            <h2>ABOUT US</h2>
+            <p>
+              {" "}
+              The first centralized IP camera, the Axis Neteye 200, was released
+              in 1996 by Axis Communications and was developed by the team of
+              Martin Gren and Carl-Axel Alm.[3] Though promoted based on its
+              direct accessibility from anywhere with an internet connection,[4]
+              the camera couldn't stream real-time motion video, but was limited
+            </p>
+          </Col>
+          <Col sm="3">
+            <h2>ABOUT US</h2>
+            <p>
+              {" "}
+              The first centralized IP camera, the Axis Neteye 200, was released
+              in 1996 by Axis Communications and was developed by the team of
+              Martin Gren and Carl-Axel Alm.[3] Though promoted based on its
+              direct accessibility from anywhere with an internet connection,[4]
+              the camera couldn't stream real-time motion video, but was limited
+            </p>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
